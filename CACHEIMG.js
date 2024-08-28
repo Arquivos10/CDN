7955249691b581ad6246340e847eea1dc8d4fc83
Arquivos10 <luispaulotop7@gmail.com>
2024-08-28 20:44:01 -0300

    Update CACHEIMG.js

--- CACHEIMG.js
+++ CACHEIMG.js
@@ -1,24 +1,34 @@
-<script>
+<script type='text/javascript'>
+//<![CDATA[
 document.querySelectorAll('img').forEach(function(img){img.src = img.src.replace(/(http|https):\/\//,'https://wsrv.nl/?url=');
 });
+//]]>
 </script> 
 
-<script>
+<script type='text/javascript'>
+//<![CDATA[
 document.querySelectorAll('img').forEach(function(img){img.src = img.src.replace(/(.webp)/,'.webp&w=1200&h=800&output=webp&ll&l=7&q=85&maxage=1y');
 });
+//]]>
 </script> 
 
-<script>
+<script type='text/javascript'>
+//<![CDATA[
 document.querySelectorAll('img').forEach(function(img){img.src = img.src.replace(/(.jpg)/,'.jpg&w=1200&h=800&output=webp&ll&l=7&q=85&maxage=1y');
 });
+//]]>
 </script> 
 
-<script>
+<script type='text/javascript'>
+//<![CDATA[
 document.querySelectorAll('img').forEach(function(img){img.src = img.src.replace(/(.png)/,'.png&w=1200&h=800&output=webp&ll&l=7&q=85&maxage=1y');
 });
+//]]>
 </script> 
 
-<script>
+<script type='text/javascript'>
+//<![CDATA[
 document.querySelectorAll('img').forEach(function(img){img.src = img.src.replace(/(.jpeg)/,'.jpeg&w=1200&h=800&output=webp&ll&l=7&q=85&maxage=1y');
 });
+//]]>
 </script> 
